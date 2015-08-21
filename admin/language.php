fb25e1424e7283d9b346e60db9e136bc301d9830
yosra <yosra46@yahoo.com>
2015-08-21 22:15:19 +0300

    admin/incHeader.php translation

--- admin/language.php
+++ admin/language.php
@@ -1,3 +1,65 @@
 <?php
 
-	$Translation['Attention!'] = 'Attention!';
+	// incHeader.php
+	
+	
+	$Translation['membership management'] = "Membership Management";
+	$Translation['password mismatch'] = "Password doesn\'t match.";
+	$Translation['error'] = "Error";
+	$Translation['invalid email'] = "Invalid Email Address";
+	$Translation['sending mails'] = "Sending mails might take some time. Please don't close this page until you see the 'Done' message.";
+	$Translation['complete step 4'] = "Please complete step 4 by selecting the member you want to transfer records to.";
+	$Translation['info'] = "Info";
+	
+	
+	
+	
+	$Translation['sure move member'] = 'Are you sure you want to move member \'<MEMBER>\' and his data from group \'<OLDGROUP>\' to group \'<NEWGROUP>\'?';
+	$Translation['sure move data of member'] = 'Are you sure you want to move data of member \'<OLDMEMBER>\' from group \'<OLDGROUP>\' to member \'<NEWMEMBER>\' from group \'<NEWGROUP>\'?';
+	
+	$Translation['sure move all members'] = 'Are you sure you want to move all members and data from group \'<OLDGROUP>\' to group \'<NEWGROUP>\'?';
+	$Translation['sure move data of all members'] = 'Are you sure you want to move data of all members of group \'<OLDGROUP>\' to member \'<MEMBER>\' from group \'<NEWGROUP>\'?';
+	
+	
+	$Translation['toggle navigation'] = "Toggle navigation";
+	$Translation['admin area'] = "Admin Area";
+	$Translation['groups'] = "Groups";
+	$Translation['view groups'] = "View Groups";
+	$Translation['add group'] = "Add Group";
+	$Translation['edit anonymous permissions'] = "Edit Anonymous Permissions";
+	$Translation['members'] = "Members";
+	$Translation['view members'] = "View Members";
+	$Translation['add member'] = "Add Member";
+	$Translation["view members' records"] = "View Members\' Records";  
+	$Translation["utilities"] = "Utilities"; 
+	$Translation["admin settings"] = "Admin Settings"; 
+	$Translation["rebuild thumbnails"] = "Rebuild Thumbnails"; 
+	$Translation['rebuild fields'] = "Rebuild fields";
+	$Translation['import CSV'] = "Import CSV data";
+	$Translation['batch transfer'] = "Batch Transfer Wizard";
+	$Translation['mail all users'] = "Mail All Users";
+	$Translation['AppGini forum'] = "AppGini Community Forum";
+	$Translation["user's area"] = "User\'s area";
+	$Translation["sign out"] = "Sign out";
+	
+	
+	$Translation["attention"] = "Attention!";
+	$Translation['security risk admin'] = "You are using the default admin username and password. This is a huge security risk. Please change at least the admin password from the <ADMINLINK> page <em>immediately</em>." ;
+	$Translation['security risk'] = "You are using the default admin password. This is a huge security	risk. Please change the admin password from the <ADMINLINK> page <em>immediately</em>." ;
+	
+	
+	
+	
+
+	
+
+	
+	
+
+
+
+
+
+
+
+?>
