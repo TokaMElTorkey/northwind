2005e43be208877e6f40a865ab8ae9201c1931c3
yosra <yosra46@yahoo.com>
2015-08-22 14:14:47 +0300

    admin/pageAssignOwners.php translation

--- admin/language.php
+++ admin/language.php
@@ -1,8 +1,6 @@
 <?php
 
 	// incHeader.php
-	
-	
 	$Translation['membership management'] = "Membership Management";
 	$Translation['password mismatch'] = "Password doesn\'t match.";
 	$Translation['error'] = "Error";
@@ -11,16 +9,12 @@
 	$Translation['complete step 4'] = "Please complete step 4 by selecting the member you want to transfer records to.";
 	$Translation['info'] = "Info";
 	
-	
-	
-	
 	$Translation['sure move member'] = 'Are you sure you want to move member \'<MEMBER>\' and his data from group \'<OLDGROUP>\' to group \'<NEWGROUP>\'?';
 	$Translation['sure move data of member'] = 'Are you sure you want to move data of member \'<OLDMEMBER>\' from group \'<OLDGROUP>\' to member \'<NEWMEMBER>\' from group \'<NEWGROUP>\'?';
 	
 	$Translation['sure move all members'] = 'Are you sure you want to move all members and data from group \'<OLDGROUP>\' to group \'<NEWGROUP>\'?';
 	$Translation['sure move data of all members'] = 'Are you sure you want to move data of all members of group \'<OLDGROUP>\' to member \'<MEMBER>\' from group \'<NEWGROUP>\'?';
-	
-	
+		
 	$Translation['toggle navigation'] = "Toggle navigation";
 	$Translation['admin area'] = "Admin Area";
 	$Translation['groups'] = "Groups";
@@ -42,10 +36,31 @@
 	$Translation["user's area"] = "User\'s area";
 	$Translation["sign out"] = "Sign out";
 	
-	
 	$Translation["attention"] = "Attention!";
 	$Translation['security risk admin'] = 'You are using the default admin username and password. This is a huge security risk. Please change at least the admin password from the <a href="pageSettings.php">Admin Settings</a> page <em>immediately</em>.';
 	$Translation['security risk'] = 'You are using the default admin password. This is a huge security	risk. Please change the admin password from the <a href="pageSettings.php">Admin Settings</a> page <em>immediately</em>.' ;
+	
+	
+	//pageAssignOwners.php
+	$Translation["assigned table records to group"] = "Assigned <NUMBER> records of table '<TABLE>' to group '<GROUP>'";
+	$Translation["assigned table records to group and member"] = "Assigned <NUMBER> records of table '<TABLE>' to group '<GROUP>' , member '<MEMBERID>'";
+	
+	$Translation['data ownership assign'] = "Assign ownership to data that has no owners";
+	$Translation['records ownership done'] = "All records in all tables have owners now.<br>Back to <a href=\"pageHome.php\">Admin homepage</a>.";
+	$Translation['select group'] = "Select group";
+	$Translation['data ownership'] = "Sometimes, you might have tables with data that were entered before implementing this AppGini membership management system, or entered using other applications unaware of AppGini ownership system. This data currently has no owners. This page allows you to assign owner groups and owner members to this data.";
+	
+	$Translation["table"] = "Table";
+	$Translation["records with no owners"] = "Records with no owners";
+	$Translation["new owner group"] = "New owner group";
+	$Translation["new owner member"] = "New owner member*";	
+	$Translation["cancel"] = "Cancel";
+	$Translation["assign new owners"] = "Assign new owners";
+	$Translation["please wait"] = "Please wait ...";	
+	$Translation["if no owner member assigned"] = '* If you assign no owner member here, you can still use the <a href="pageTransferOwnership.php">Batch Transfer Wizard</a> later to do so.';
+
+	
+	
 	
 	
 	
